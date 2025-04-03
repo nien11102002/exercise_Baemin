5edd756bc55e9b8403ffec4fa62de38a9b355732
nien11102002 <nguyenduynien11102002@gmail.com>
2025-04-04 01:12:56 +0700

    setup prisma and microservice config

--- baemin_microservice/api_gateway/src/app.module.ts
+++ baemin_microservice/api_gateway/src/app.module.ts
@@ -1,9 +1,67 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ProductModule } from './modules/product/product.module';
+import { UserModule } from './modules/user/user.module';
+import { OrderModule } from './modules/order/order.module';
+import { ShippingModule } from './modules/shipping/shipping.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_NAME',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://admin:1234@localhost:5672'],
+          queue: 'product_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+      {
+        name: 'USER_NAME',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://admin:1234@localhost:5672'],
+          queue: 'user_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+      {
+        name: 'SHIPPING_NAME',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://admin:1234@localhost:5672'],
+          queue: 'shipping_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+      {
+        name: 'ORDER_NAME',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://admin:1234@localhost:5672'],
+          queue: 'order_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+    ProductModule,
+    UserModule,
+    OrderModule,
+    ShippingModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
